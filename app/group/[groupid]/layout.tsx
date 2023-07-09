ec9e6b652fc641419fdb0bdd883174e592916bd0
Andrew Fleming <ajfleming1992@gmail.com>
2023-07-09 19:31:40 +0100

    GroupProvider broke as I forgot about Params

--- app/group/[groupid]/layout.tsx
+++ app/group/[groupid]/layout.tsx
@@ -4,16 +4,18 @@ import AuthWrapper from "@/app/components/Auth/AuthWrapper"
 import ContainerWrapper from "@/app/components/ContainerWrapper"
 import GroupPageWrapper from "./components/GroupPageWrapper"
 
+//Params = Query Params from NextJS
 export interface GroupLayoutProps {
     children: ReactNode,
-    groupId: string
+    groupId: string,
+    params: any
 }
 
-const GroupLayout = async ({children, groupId}: GroupLayoutProps) => {
+const GroupLayout = async ({children, params}: GroupLayoutProps) => {
     return (
         <AuthWrapper required={true}>
             <ContainerWrapper width="6xl">
-                <GroupProvider groupId={parseInt(groupId)}> 
+                <GroupProvider groupId={parseInt(params.groupid)}> 
                     <GroupPageWrapper>
                         {children}
                     </GroupPageWrapper>
