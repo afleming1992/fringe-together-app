225c1311de61a5ce479e6d2660ee2218bc221b42
Andrew Fleming <ajfleming1992@gmail.com>
2023-07-23 18:26:44 +0100

    Fix Charset Bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,10 +30,10 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
-        <meta name="description" content="description of your project" />
+        <meta name="description" content="Planning your fringe with friends, together" />
         <meta name="theme-color" content="#000" />
         <title>Title of the project</title>
         <link rel="manifest" href="/manifest.json" />
